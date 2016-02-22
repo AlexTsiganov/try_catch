33f1498db3a8a4a04b74652dc1d105ad9517517b
Igor Brodt <programist.igor@rambler.ru>
2016-02-22 15:39:53 +0300

    Полностью залит движок CodeIgniter

---
+++ http/application/views/show_chart1.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Гистограмма</title>
+</head>
+<body>
+    test
+</body>
+</html>
