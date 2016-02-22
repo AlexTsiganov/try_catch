6b6ff6b9c5dab69e638eb3fd372f2b7d04cd322d
Zhuk Pavel <ppzhuk@gmail.com>
2016-02-22 16:40:14 +0300

    add a few insertion scripts

---
+++ Database/insertion_scripts/1_categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO sales_test_db.category (name, pid) VALUES ('Интернет', null);
+INSERT INTO sales_test_db.category (name, pid) VALUES ('Телевидение', null);
+INSERT INTO sales_test_db.category (name, pid) VALUES ('Цифровое телевидение', null);
+INSERT INTO sales_test_db.category (name, pid) VALUES ('Телефония', null);
